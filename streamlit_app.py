f035a93e49a4cbca3729c434ee43ba0b43eb5a46
Mohamed Rasvi <115227087+mrasvit@users.noreply.github.com>
2022-10-07 16:17:22 +0200

    Update streamlit_app.py
    Added header Menu and text of dishes list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Menu')
+streamlit.text('Boiled Egg')
+streamlit.text('Shawarma')
+streamlit.text('Chicken Grill')
+streamlit.text('Chicken Biryani')
