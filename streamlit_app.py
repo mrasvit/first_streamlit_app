e9ed765dad3f6268ff1d7d9c12c5c99f2c4e12e2
Mohamed Rasvi <115227087+mrasvit@users.noreply.github.com>
2022-10-11 14:25:37 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,18 +25,24 @@ fruits_to_show=my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvice fruit Advice')
+try:
+    fruit_choice = streamlit.text_input('what fruit would you like information about?','Kiwi')
+    if fruit_choice:
+        streamlit.write('The user entered',fruit_choice)
 
-fruit_choice = streamlit.text_input('what fruit would you like information about?','Kiwi')
-streamlit.write('The user entered',fruit_choice)
+        #import requests
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+        #just writes the data on the screen
+        #streamlit.text(fruityvice_response.json())
 
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-#just writes the data on the screen
-#streamlit.text(fruityvice_response.json())
-
-#to display json data in the tabular format
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
+        #to display json data in the tabular format
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+        streamlit.dataframe(fruityvice_normalized)
+    else:
+        streamlit.error('Please select a fruit to get information.')
+        
+except URLError as e:
+    streamlit.error()
 
 streamlit.stop()
 
